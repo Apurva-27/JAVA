b52036c4a82485ffc4d11ace9d842fa6ed38d61a
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-01-28 15:17:54 +0530

    Add files via upload

---
+++ for_loop_example.java
@@ -0,0 +1,15 @@
+
+public class for_loop_example {
+
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for(i=1; i<=5; i++)
+		{
+			System.out.println("Hello Word.This is example of for loop.");
+		}
+
+	}
+
+}
