faa8187325a69c8de2a9acd73657a97850248d71
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-01-28 17:14:18 +0530

    Add files via upload

---
+++ even_num.java
@@ -0,0 +1,17 @@
+
+public class even_num {
+
+	public static void main(String[] args) {
+	
+		int i, n;
+		
+		for(i=1; i<=50; i++)
+		{
+			if(i%2==0)
+			{	
+				System.out.println(i);
+			}		
+		}
+	}
+
+}
