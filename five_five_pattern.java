ddf25c2d193fa9abea0431dbb1bc3f81d60aa7d7
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-02-02 16:57:03 +0530

    Add files via upload

---
+++ five_five_pattern.java
@@ -0,0 +1,18 @@
+
+public class five_five_pattern {
+
+	public static void main(String[] args) {
+		int i, j;
+		
+		for(i=1; i<=5; i++)
+		{
+			for(j=1; j<=5; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
