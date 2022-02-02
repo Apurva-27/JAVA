b7a32cdc15667a1e126cafaac7ed5579bcfab7dd
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-02-02 17:23:57 +0530

    Add files via upload

---
+++ pattern_two.java
@@ -0,0 +1,27 @@
+
+public class pattern_two {
+
+	public static void main(String[] args) {
+		int i, j;
+		
+		for(i=1; i<=5; i++)
+		{
+			for(j=i; j>=1; j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+/*
+
+*
+**
+***
+****
+*****
+
+*/
