110930e41234d28b32506edfe19b9105aabaf251
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-02-10 17:42:48 +0530

    Update singleexample.java

--- inheritance/singleexample.java
+++ inheritance/singleexample.java
@@ -3,7 +3,7 @@ package Inheritance;
 class Bank
 {
 	String Bank_name;
-	
+	int x=10;
 	public Bank(String Bank_name)
 	{
 		this.Bank_name=Bank_name;
@@ -18,6 +18,7 @@ class Bank
 
 class sbi extends Bank
 {
+	int x=100;
 	public sbi(String Bank_name)
 	{
 		super(Bank_name);//call parent class constructor 
@@ -26,7 +27,11 @@ class sbi extends Bank
 	
 	void display()
 	{
+		int x=200; 
 		System.out.println("Bank Name :"+Bank_name);
+		System.out.println("display:"+x);
+		System.out.println("child:"+this.x);
+		System.out.println("parent:"+super.x);
 	}//display
 }//child class
 
