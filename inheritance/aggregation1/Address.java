3ec0972dc60dcfe07004ea362da564a358e75241
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-02-12 17:55:00 +0530

    Create Address.java

---
+++ inheritance/aggregation1/Address.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class Address {
+	String city,state,country;
+	
+	public Address(String city, String state, String country)
+		{
+			super();
+			this.city=city;
+			this.country=country;
+			this.state=state;
+		}
+		
+	}
+
+
