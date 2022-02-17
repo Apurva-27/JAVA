3c9eaad668f9f51f2b9333c29bd2616facc484e8
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-02-17 14:33:23 +0530

    Add files via upload

---
+++ String_Example/SringBufferexample.java
@@ -0,0 +1,19 @@
+package StringHandaling;
+
+public class SringBufferexample {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sBuffer = new StringBuffer("testing");
+		
+		sBuffer.append("String Buffers");
+		
+		System.out.println(sBuffer);
+	}
+
+}
+
+/*
+ output
+testingString Buffers
+*/
