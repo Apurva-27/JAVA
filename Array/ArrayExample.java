d668172f87fcf58f48610fab7278b97d0b930bb4
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-02-18 15:16:50 +0530

    Update ArrayExample.java

--- Array/ArrayExample.java
+++ Array/ArrayExample.java
@@ -1,8 +1,13 @@
 package Array;
 
+import java.util.*;
+
 public class ArrayExample {
 
 	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		
 		int i;
 		//compile time initializing arry e.g
 		int arr[]= {2,3,4,5,6};
@@ -14,12 +19,29 @@ public class ArrayExample {
 		arr1[3]=8;
 		arr1[4]=9;
 		
+		System.out.print("\nEnter Size of the array:");
+		int N = sc.nextInt();
+		int arr2[]=new int[N];		
+		//OR
+		System.out.print("\nEnter "+N+"in Array;");
+		for(int k=0; k<N; k++)
+		{
+			arr2[k]=sc.nextInt();	
+		}
+		
 		//traversing array
 		System.out.print("\n"+arr[1]);	//3
 		
 		for(i=0; i<5; i++)
 		{
 			System.out.print("\n"+arr1[i]);	
+		}
+		
+		//for each loop
+		//for(datatype variable:array) {sout(variable);}
+		for(int j:arr1)
+		{
+			System.out.print("\n"+j);
 		}
 	}
 
