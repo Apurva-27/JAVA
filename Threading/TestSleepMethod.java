7bffcb71beeb5380d202472c490d7137c136d06d
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-03-07 16:29:16 +0530

    Update TestSleepMethod.java

--- Threading/TestSleepMethod.java
+++ Threading/TestSleepMethod.java
@@ -14,16 +14,26 @@ public class TestSleepMethod extends Thread{
 			{
 				System.out.println(e);
 			}
-			System.out.println(i);
+			//System.out.println(i);
+			System.out.println(Thread.currentThread().getName()+" = "+ i);
 		}
 	}
 	
 	public static void main(String[] args) {
 		TestSleepMethod t1 = new TestSleepMethod();
 		TestSleepMethod t2 = new TestSleepMethod();
+		TestSleepMethod t3= new TestSleepMethod();
 		
 		t1.start();
+		try {
+			t1.join(1500);
+		}
+		catch (InterruptedException ee){
+			System.out.println(ee);
+		}//t1.start();//IllegalThreadStateException
+		
 		t2.start();
+		t3.start();
 	}
 
 }
