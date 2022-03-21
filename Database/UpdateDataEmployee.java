fadde48a729ad98079c12104ed1baf2e06c4e254
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-03-21 14:56:30 +0530

    Update UpdateDataEmployee.java

--- Database/UpdateDataEmployee.java
+++ Database/UpdateDataEmployee.java
@@ -2,6 +2,7 @@ package com.jdbc;
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class UpdateDataEmployee {
 
@@ -13,9 +14,19 @@ public class UpdateDataEmployee {
 			//create statement
 			Statement stmt = conn.createStatement();
 			
-			//create query
-			String query="update emp set ename='ajith',sal=45000 where eid=101";
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Emp name:");
+			String empname=sc.nextLine();
 			
+			System.out.println("Enter Emp salary:");
+			int empsal=sc.nextInt();
+			
+			
+			String query="update emp set ename='"+empname+"',sal="+empsal+" where eid=101";
+			
+			//create query
+			/*String query="update emp set ename='ajith',sal=45000 where eid=101";
+			*/
 			int row=stmt.executeUpdate(query);
 			
 			System.out.println(row+" Record updated Sucessfully....");
